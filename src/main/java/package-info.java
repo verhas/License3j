3c689fc9682aba0594057a793da83de326ea6779
Peter Verhas <peter@verhas.com>
2017-06-11 02:06:36 +0200

    package html converted to package-info.java

---
+++ src/main/java/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the License3j class that contains a
+ * public static main method to start license geneartion. This class
+ * is in the default package to ease the execution of the licensor
+ * from the command line and let you avoid write long package names
+ * on the command line.
+ */
