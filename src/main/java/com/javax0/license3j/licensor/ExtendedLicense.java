095e57278b041713f0f06b379036265dfcf40eda
Greg Schueler <greg.schueler@gmail.com>
2017-08-04 09:11:40 -0700

    Fix setting hour of day in expiration check
    The `Calendar.HOUR` sets the hour of am/pm, so setting to 0 could set to 12pm instead of 12am, the 
    correct field is [HOUR_OF_DAY](https://docs.oracle.com/javase/7/docs/api/java/util/Calendar.html#HOUR_OF_DAY)

--- src/main/java/com/javax0/license3j/licensor/ExtendedLicense.java
+++ src/main/java/com/javax0/license3j/licensor/ExtendedLicense.java
@@ -52,7 +52,7 @@ public class ExtendedLicense extends License {
     try {
       expiryDate = getFeature(EXPIRATION_DATE, Date.class);
       final GregorianCalendar today = new GregorianCalendar();
-      today.set(Calendar.HOUR, 0);
+      today.set(Calendar.HOUR_OF_DAY, 0);
       today.set(Calendar.MINUTE, 0);
       today.set(Calendar.SECOND, 0);
       today.set(Calendar.MILLISECOND, 0);
