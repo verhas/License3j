5ed87487e86c4648d980938981076ec585809783
Peter Verhas <peter@verhas.com>
2018-05-05 16:14:50 +0200

    package info is not needed since we do not have any classes in the default package methods that were not used in SourceLine were deleted and another one that was used but did not exist due to some merge error/mistake javadoc was commented out from the build becuase of some version incompatibility that I could not overcome

--- src/test/java/com/javax0/license3j/filecompare/SourceLine.java
+++ src/test/java/com/javax0/license3j/filecompare/SourceLine.java
@@ -1,36 +1,32 @@
 package com.javax0.license3j.filecompare;
 
 class SourceLine implements Comparable<SourceLine> {
-  final private String line;
+    final private String line;
 
-  SourceLine(String line) {
-    this.line = line;
-  }
+    SourceLine(String line) {
+        this.line = line;
+    }
 
-  boolean eof() {
-    return line == null;
-  }
+    boolean eof() {
+        return line == null;
+    }
 
-  boolean isNotComment() {
-    return line == null || !line.startsWith("#");
-  }
+    boolean isComment() {
+        return line != null && line.startsWith("#");
+    }
 
-  boolean doesNotEqual(Object other) {
-    return !equals(other);
-  }
+    @Override
+    public boolean equals(Object other) {
+        return (line == null && ((SourceLine) other).line == null)
+            || (line != null && line.equals(((SourceLine) other).line));
+    }
 
-  @Override
-  public boolean equals(Object other) {
-    return (line == null && ((SourceLine) other).line == null)
-        || (line != null && line.equals(((SourceLine) other).line));
-  }
+    public String toString() {
+        return line;
+    }
 
-  public String toString() {
-    return line;
-  }
-
-  @Override
-  public int compareTo(SourceLine o) {
-    return line.compareTo(o.line);
-  }
+    @Override
+    public int compareTo(SourceLine o) {
+        return line.compareTo(o.line);
+    }
 }
