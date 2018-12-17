fb262c557b9f473b18c8bd345c7d7a20ad4702c7
Peter_Verhas <peter@verhas.com>
2018-12-17 17:55:36 +0100

    license deserialization

--- src/main/java/com/javax0/license3j/three/License.java
+++ src/main/java/com/javax0/license3j/three/License.java
@@ -1,5 +1,6 @@
 package com.javax0.license3j.three;
 
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,32 +13,52 @@ import java.util.Map;
  * name of rights and so on.
  */
 public class License {
-    final private Map<String,Feature> features = new HashMap<>();
-    public Feature get(String name){
+    final private Map<String, Feature> features = new HashMap<>();
+
+    public Feature get(String name) {
         return features.get(name);
     }
 
-    public Feature put(String name, Feature feature){
-        return features.put(name,feature);
+    public Feature put(String name, Feature feature) {
+        return features.put(name, feature);
     }
 
-    public byte[] serialized(){
+    public byte[] serialized() {
         final var featureNr = features.values().size();
         byte[][] featuresSerialized = new byte[featureNr][];
         var i = 0;
         var size = 0;
-        for(final var feature : features.values()){
+        for (final var feature : features.values()) {
             featuresSerialized[i] = feature.serialized();
             size += featuresSerialized[i].length;
             i++;
         }
 
         final var buffer = ByteBuffer.allocate(size + Integer.BYTES * featureNr);
-        for( final var featureSerialized :  featuresSerialized ){
+        for (final var featureSerialized : featuresSerialized) {
             buffer.putInt(featureSerialized.length);
             buffer.put(featureSerialized);
         }
         return buffer.array();
+    }
+
+    public static class Create {
+        public static License from(final byte[] array) {
+            var license = new License();
+            var buffer = ByteBuffer.wrap(array);
+            while (buffer.hasRemaining()) {
+                try {
+                    var featureLength = buffer.getInt();
+                    var featureSerialized = new byte[featureLength];
+                    buffer.get(featureSerialized);
+                    var feature = Feature.Create.from(featureSerialized);
+                    license.put(feature.name(), feature);
+                } catch (BufferUnderflowException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
+            return license;
+        }
     }
 
 }
