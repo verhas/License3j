30481c3bbc53d2bdac5ca5332e15b0cf5f4e8930
hr.stoyanov <tajna6378>
2021-10-17 12:02:11 -0700

    Added 3 additional cloud providers, code and documentation enhancements.

--- src/main/java/javax0/license3j/hardware/CloudProvider.java
+++ src/main/java/javax0/license3j/hardware/CloudProvider.java
@@ -6,15 +6,22 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
 
 /**
- * Obtain the unique and immutable cloud instance/machine id, based on the dedicated, non-routable 169.254.169.254 ip address,
- * as implemented by various cloud providers.
+ * Obtain unique and immutable cloud instance/virtual machine Id, suitable for licensing purposes.
+ * <p>
+ * Implementation note:<br>
+ * Some cloud providers implement a GET rest endpoint (based on a dedicated, non-routable ip address)
+ * that can report back the instance id easily. In some, cases such a rest endpoint is accessible with special tokens only.
+ * In other cases, specific OS specific commands exists to obtain the instance id.
+ * </p>
  */
 public enum CloudProvider {
 
     /**
      * Obtain a Azure instance id.
+     * <p>
      * Refer to <a href="https://docs.microsoft.com/en-us/azure/virtual-machines/windows/instance-metadata-service?tabs=windows">Azure documentation</a>
      * Refer to <a href="https://gist.github.com/dreamorosi/50cbfd622b478c2433602c16b7321c5d">Examples</a>
      */
@@ -27,6 +34,7 @@ public enum CloudProvider {
 
     /**
      * Obtain a AWS instance id.
+     * <p>
      * Refer to <a href="https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html">AWS documentation</a>
      * Refer to <a href="https://gist.github.com/dreamorosi/50cbfd622b478c2433602c16b7321c5d">Examples</a>
      * Refer to <a href="https://stackoverflow.com/questions/625644/how-to-get-the-instance-id-from-within-an-ec2-instance">Stack overflow</a>
@@ -48,23 +56,79 @@ public enum CloudProvider {
     Google {
         @Override
         public String getInstanceId() {
-            return instanceIdFor("http://169.254.169.254/computeMetadata/v1/instance?alt=text", "Metadata-Flavor", "Google");
+            return instanceIdFor("http://metadata.google.internal/computeMetadata/v1/instance?alt=text", "Metadata-Flavor", "Google");
+        }
+    },
+
+    /**
+     * Obtain an Oracle cloud instance id.
+     * <p>
+     * Refer to <a href="https://docs.oracle.com/en-us/iaas/Content/Compute/Tasks/gettingmetadata.htm#metadata-keys">Oracle Cloud docs</a>
+     * Refer to <a href="https://docs.oracle.com/en-us/iaas/Content/Compute/Tasks/gettingmetadata.htm#metadata-keys">Oracle Cloud metadata keys</a>
+     * <p/>
+     */
+    Oracle {
+        @Override
+        public String getInstanceId() {
+            return instanceIdFor("http://169.254.169.254/opc/v2/instance/id", "Authorization", "Bearer Oracle");
+        }
+    },
+
+    /**
+     * Obtain an Digital Ocean instance id.
+     * <p>
+     * Refer to <a href="https://www.alibabacloud.com/help/doc-detail/108460.htm?spm=a2c63.p38356.b99.196.4e3b3828rFutRB">How to Access Droplet Metadata</a>
+     * </p>
+     */
+    DigitalOcean {
+        @Override
+        public String getInstanceId() {
+            return instanceIdFor("http://169.254.169.254/metadata/v1/id", (String[]) null);
+        }
+    },
+
+    /**
+     * Obtain Alibaba cloud instance id.
+     * <p>
+     * Refer to <a href="https://www.alibabacloud.com/help/doc-detail/108460.htm?spm=a2c63.p38356.b99.196.4e3b3828rFutRB">View instance metadata</a>
+     * </p>
+     */
+    AliBaba {
+        @Override
+        public String getInstanceId() {
+            return instanceIdFor("http://100.100.100.200/latest/meta-data/instance-id", (String[]) null);
         }
     };
 
-
+    /**
+     * Get instance id, but can throw run-time exception.
+     *
+     * @return the instance id.
+     */
     abstract public String getInstanceId();
+
+    /**
+     * Get the instance id.
+     *
+     * @return the instance id or NULL if there is a problem getting it.
+     */
+    public String getInstanceIdIgnoreException() {
+        try {
+            return getInstanceId();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 
     private static String instanceIdFor(String instanceIdUrl, String... headers) {
+        Objects.requireNonNull(instanceIdUrl);
         try {
             HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request =
-                    HttpRequest
-                            .newBuilder(new URI(instanceIdUrl))
-                            .headers(headers)
-                            .GET()
-                            .build();
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(new URI(instanceIdUrl));
+            if (headers != null && headers.length > 0)
+                requestBuilder.headers(headers);
+            HttpRequest request = requestBuilder.GET().build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             return response.body();
         } catch (InterruptedException | URISyntaxException | IOException e) {
