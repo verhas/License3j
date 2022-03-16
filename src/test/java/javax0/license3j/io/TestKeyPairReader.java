c0435151984d120199cc1df7d1952e29ad99d862
Peter Verhas <peter@verhas.com>
2022-03-16 20:31:47 +0100

    test refactored to handle developer directory name containing space or other special character that mainly happens when a developer is using Windows

--- src/test/java/javax0/license3j/io/TestKeyPairReader.java
+++ src/test/java/javax0/license3j/io/TestKeyPairReader.java
@@ -4,16 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Objects;
 
 public class TestKeyPairReader {
+
+    private KeyPairReader getSut(final String publicKeyBinary) throws FileNotFoundException {
+        final URL url = getClass().getResource(publicKeyBinary);
+        Objects.requireNonNull(url, () -> String.format("The resource %s was not found", publicKeyBinary));
+        return new KeyPairReader(URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8));
+    }
 
     @Test
     @DisplayName("Can read public key from BINARY file")
     void canReadPublicKeyFromBinaryFile() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        try (final var sut = new KeyPairReader(this.getClass().getResource(TestKeyPairWriter.PUBLIC_KEY_BINARY).getFile())) {
+        try (final var sut = getSut(TestKeyPairWriter.PUBLIC_KEY_BINARY)) {
             final var key = sut.readPublic();
             Assertions.assertEquals("RSA", key.cipher());
         }
@@ -22,7 +33,7 @@ public class TestKeyPairReader {
     @Test
     @DisplayName("Can read private key from BINARY file")
     void canReadPrivateKeyFromBinaryFile() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        try (final var sut = new KeyPairReader(this.getClass().getResource(TestKeyPairWriter.PRIVATE_KEY_BINARY).getFile())) {
+        try (final var sut = getSut(TestKeyPairWriter.PRIVATE_KEY_BINARY)) {
             final var key = sut.readPrivate();
             Assertions.assertEquals("RSA", key.cipher());
         }
@@ -31,7 +42,7 @@ public class TestKeyPairReader {
     @Test
     @DisplayName("Can read public key from BASE64 file")
     void canReadPublicKeyFromBase64File() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        try (final var sut = new KeyPairReader(this.getClass().getResource(TestKeyPairWriter.PUBLIC_KEY_BASE64).getFile())) {
+        try (final var sut = getSut(TestKeyPairWriter.PUBLIC_KEY_BASE64)) {
             final var key = sut.readPublic(IOFormat.BASE64);
             Assertions.assertEquals("RSA", key.cipher());
         }
@@ -40,7 +51,7 @@ public class TestKeyPairReader {
     @Test
     @DisplayName("Can read private key from BASE64 file")
     void canReadPrivateKeyFromBase64File() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        try (final var sut = new KeyPairReader(this.getClass().getResource(TestKeyPairWriter.PRIVATE_KEY_BASE64).getFile())) {
+        try (final var sut = getSut(TestKeyPairWriter.PRIVATE_KEY_BASE64)) {
             final var key = sut.readPrivate(IOFormat.BASE64);
             Assertions.assertEquals("RSA", key.cipher());
         }
@@ -49,7 +60,7 @@ public class TestKeyPairReader {
     @Test
     @DisplayName("Throws exception when trying to read key file using STRING format")
     void throwsUpOnStringFormat() throws IOException {
-        try (final var sut = new KeyPairReader(this.getClass().getResource(TestKeyPairWriter.PRIVATE_KEY_BASE64).getFile())) {
+        try (final var sut = getSut(TestKeyPairWriter.PRIVATE_KEY_BASE64)) {
             Assertions.assertThrows(IllegalArgumentException.class, () -> sut.readPrivate(IOFormat.STRING));
         }
     }
--- src/test/java/javax0/license3j/io/TestLicenseReader.java
+++ src/test/java/javax0/license3j/io/TestLicenseReader.java
@@ -4,14 +4,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class TestLicenseReader {
+
+
+    private LicenseReader getSut(final String fileText, int limit) throws FileNotFoundException {
+        final URL url = getClass().getResource(fileText);
+        Objects.requireNonNull(url, () -> String.format("file %s not found", fileText));
+        if (limit < 0) {
+            return new LicenseReader(URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8));
+        } else {
+            return new LicenseReader(URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8), limit);
+        }
+    }
 
     @Test
     @DisplayName("Can read license from text file")
     void canReadLicenseFromTextFile() throws IOException {
-        try (final var sut = new LicenseReader(this.getClass().getResource(TestLicenseWriter.FILE_TEXT).getFile())) {
+        try (final var sut = getSut(TestLicenseWriter.FILE_TEXT, -1)) {
             final var lic = sut.read(IOFormat.STRING);
             Assertions.assertEquals("string feature", lic.get("simple").getString());
         }
@@ -20,7 +36,7 @@ public class TestLicenseReader {
     @Test
     @DisplayName("Can read license from binary file")
     void canReadLicenseFromBinaryFile() throws IOException {
-        try (final var sut = new LicenseReader(this.getClass().getResource(TestLicenseWriter.FILE_BINARY).getFile())) {
+        try (final var sut = getSut(TestLicenseWriter.FILE_BINARY, -1)) {
             final var lic = sut.read(IOFormat.BINARY);
             Assertions.assertEquals("string feature", lic.get("simple").getString());
         }
@@ -29,7 +45,7 @@ public class TestLicenseReader {
     @Test
     @DisplayName("Can read license from binary file using default read()")
     void canReadDefaultLicenseFromBinaryFile() throws IOException {
-        try (final var sut = new LicenseReader(this.getClass().getResource(TestLicenseWriter.FILE_BINARY).getFile())) {
+        try (final var sut = getSut(TestLicenseWriter.FILE_BINARY, -1)) {
             final var lic = sut.read();
             Assertions.assertEquals("string feature", lic.get("simple").getString());
         }
@@ -38,7 +54,7 @@ public class TestLicenseReader {
     @Test
     @DisplayName("Can read license from base64 file")
     void canReadLicenseFromBase64File() throws IOException {
-        try (final var sut = new LicenseReader(this.getClass().getResource(TestLicenseWriter.FILE_BASE64).getFile())) {
+        try (final var sut = getSut(TestLicenseWriter.FILE_BASE64, -1)) {
             final var lic = sut.read(IOFormat.BASE64);
             Assertions.assertEquals("string feature", lic.get("simple").getString());
         }
@@ -47,7 +63,6 @@ public class TestLicenseReader {
     @Test
     @DisplayName("Avoids reading too long file")
     void avoidTooLongFile() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> new LicenseReader(this.getClass().getResource(TestLicenseWriter.FILE_TEXT).getFile(), 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> getSut(TestLicenseWriter.FILE_TEXT, 1));
     }
 }
